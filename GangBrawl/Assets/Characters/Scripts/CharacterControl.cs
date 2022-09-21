a980cc49dee4401c1db940c9abda8f2bea488773
BeadsInCheese <36571102+BeadsInCheese@users.noreply.github.com>
2022-09-21 18:21:48 +0300

    Implemented basic movement
    Created basic movement controller and action based inputs.

--- GangBrawl/Assets/Characters/Scripts/CharacterControl.cs
+++ GangBrawl/Assets/Characters/Scripts/CharacterControl.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterControl : MonoBehaviour
 {
     // Start is called before the first frame update
+    Rigidbody2D physicsBody;
+
+    float speed=3f;
+    float jumpHeight=400;
+    Vector2 vel=new Vector2(0,0);
+    PlayerInput playerInput;
+
+    //jump Control
+   bool isgrounded=false;
+   bool doubleJump=true;
+
+ void OnCollisionEnter2D(Collision2D theCollision)
+ {
+     if (theCollision.gameObject.tag == "Platform")
+     {
+        doubleJump=true;
+         isgrounded = true;
+     }
+ }
+ 
+ void OnCollisionExit2D(Collision2D theCollision)
+ {
+     if (theCollision.gameObject.tag == "Platform")
+     {
+         isgrounded = false;
+     }
+ }
+
     void Start()
     {
-        
+        playerInput=GetComponent<PlayerInput>();
+        physicsBody=GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(playerInput.actions["Jump"].triggered){
+            if(isgrounded || doubleJump){
+                physicsBody.AddForce(new Vector2(0,jumpHeight));
+            }
+            
+        }
+        vel.x=playerInput.actions["Walk"].ReadValue<float>()*speed;
+        physicsBody.AddRelativeForce(vel);
         
+
     }
 }
