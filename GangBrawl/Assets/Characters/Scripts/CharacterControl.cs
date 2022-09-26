6d42ae527a60cc14b667b96e902e044dfbb66083
BeadsInCheese <36571102+BeadsInCheese@users.noreply.github.com>
2022-09-26 19:35:12 +0300

    Character controller update
    Updated character controller and added temporary art.

--- GangBrawl/Assets/Characters/Scripts/CharacterControl.cs
+++ GangBrawl/Assets/Characters/Scripts/CharacterControl.cs
@@ -8,10 +8,12 @@ public class CharacterControl : MonoBehaviour
     // Start is called before the first frame update
     Rigidbody2D physicsBody;
 
-    float speed=3f;
+    float speed=6f;
     float jumpHeight=400;
     Vector2 vel=new Vector2(0,0);
     PlayerInput playerInput;
+    bool spriteFlipped=false;
+
 
     //jump Control
    bool isgrounded=false;
@@ -51,10 +53,19 @@ public class CharacterControl : MonoBehaviour
                 doubleJump=false;
             }
             
-        }
-        vel.x=playerInput.actions["Walk"].ReadValue<float>()*speed;
-        physicsBody.AddRelativeForce(vel);
-        
 
+        }
+        
+        vel.y=physicsBody.velocity.y;
+        if(isgrounded){
+            vel.x=playerInput.actions["Walk"].ReadValue<float>()*speed;
+        }else{
+            vel.x=Mathf.Max(Mathf.Min(vel.x+playerInput.actions["Walk"].ReadValue<float>(),speed),-speed);
+        }
+        if(vel.x!=0){
+            spriteFlipped = vel.x>0;
+        }
+        transform.rotation=Quaternion.Euler(new Vector3(0f,spriteFlipped ? 180:0f,0f));
+        physicsBody.velocity=vel;
     }
 }
