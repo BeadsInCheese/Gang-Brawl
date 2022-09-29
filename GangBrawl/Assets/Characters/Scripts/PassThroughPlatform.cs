6fc0f1567bc3c9d10ff54ea0944547d7ecd2124c
Juuso Rytilahti <rytilahti.juuso@gmail.com>
2022-09-29 14:31:58 +0300

    Falling through platform basic script #6
    - Not yet fully working.

---
+++ GangBrawl/Assets/Characters/Scripts/PassThroughPlatform.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/** 
+ * This class is currently unused and all of it's functionality is in character controller class.
+ */
+public class PassThroughPlatform : MonoBehaviour
+{
+    private Collider2D _collider;
+    private bool _playerOnPlarform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _collider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_playerOnPlarform && Input.GetAxis("Vertical") < 0)
+        {
+            _collider.enabled = false;
+            StartCoroutine(EnableCollider());
+        }
+    }
+
+    private IEnumerator EnableCollider()
+    {
+        yield return new WaitForSeconds(0.5f);
+        _collider.enabled = true;
+    }
+
+    private void setPlayerOnPlatform(Collision2D other, bool value)
+    {
+        Debug.Log("PassThroughPlatform script");
+        var player = other.gameObject.GetComponent<CharacterControl>();
+        if (player != null)
+        {
+            _playerOnPlarform = value;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        setPlayerOnPlatform(other, true);
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        setPlayerOnPlatform(other, false);
+    }
+}
