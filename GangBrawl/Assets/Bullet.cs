7dc92f734e5b31fbdf762b027b52fa5b1ca3a934
Juuso Rytilahti <rytilahti.juuso@gmail.com>
2022-10-06 22:59:30 +0300

    Bullet can now hit the player (does not know friendly fire) #1
    - Does not recognize if the hit player is the player that shot the bullet.
    -  Also if bullet hits something that has a collider, the bullet is destroyed.

--- GangBrawl/Assets/Bullet.cs
+++ GangBrawl/Assets/Bullet.cs
@@ -6,6 +6,23 @@ public class Bullet : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D rb;
+    public int damage = 10;
+    public float knockback = 10;
+    // Start is called before the first frame update
+
+    // Directly taken from meleeAttack.cs Should probably be changed to use the same code instead of copy paste
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject col = collision.gameObject;
+        if (col.tag.Equals("Player"))
+        {
+            col.GetComponent<HPSystem>().takeDamage(damage);
+            col.GetComponent<Rigidbody2D>().AddForce(this.transform.rotation.eulerAngles.y != 0 ? new Vector2(knockback, knockback) : new Vector2(-knockback, knockback));
+        }
+        Destroy(gameObject);
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
