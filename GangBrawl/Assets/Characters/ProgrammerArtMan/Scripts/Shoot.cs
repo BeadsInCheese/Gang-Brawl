36cf9e96aab4d00e922f2b938b173f600fc05e81
BeadsInCheese <36571102+BeadsInCheese@users.noreply.github.com>
2022-10-10 23:43:21 +0300

    Refactored shoot script and improved aiming

--- GangBrawl/Assets/Characters/ProgrammerArtMan/Scripts/Shoot.cs
+++ GangBrawl/Assets/Characters/ProgrammerArtMan/Scripts/Shoot.cs
@@ -11,6 +11,9 @@ public class Shoot : MonoBehaviour
     private PlayerInput playerInput;
     private CharacterControl charControl;
 
+    public Transform shootingArm;
+    public Transform body;
+    
     private float x, y;
     double radians, angle;
 
@@ -20,6 +23,7 @@ public class Shoot : MonoBehaviour
         //playerInput = GetComponent<PlayerInput>();
         playerInput = gameObject.GetComponentInParent<PlayerInput>();
         charControl = gameObject.GetComponentInParent<CharacterControl>();
+        
     }
 
     // Update is called once per frame
@@ -27,32 +31,42 @@ public class Shoot : MonoBehaviour
     {
         x = playerInput.actions["Aim"].ReadValue<Vector2>().x;
         y = playerInput.actions["Aim"].ReadValue<Vector2>().y;
+        
         //Debug.Log(playerInput.actions["Aim"].ReadValue<Vector2>().x);
         radians = Math.Atan2(y - 0, x - 0);
         angle = radians * (180 / Math.PI);
         // angle is zero if player is currently aiming
-        if (isPlayerAiming(playerInput) && angle > -90 && angle < 90)
+    if (isPlayerAiming(playerInput)){
+        shootingArm.position=new Vector2(body.position.x+(MathF.Cos((float)radians)),body.position.y+(MathF.Sin((float)radians)));
+        shootingPoint.position=new Vector2(shootingArm.position.x+(MathF.Cos((float)radians)),shootingArm.position.y+(MathF.Sin((float)radians)));
+        if ( angle > -90 && angle < 90)
         {
+
             charControl.setSpriteFlipped(true);
         }
         else
         {
             // Character is aiming left
-            if (isPlayerAiming(playerInput))
-            {
+            
                 // Character sprite should only be flipped here if player is aiming 
                 // (aiming sprite flipping overrides flip character by which direction character is moving)
                 charControl.setSpriteFlipped(false);
-            }
+                
+            
+        }
+        }else{
+            shootingArm.position=body.position+body.up;
+            shootingPoint.position=shootingArm.position+body.up*2;
         }
         if (playerInput.actions["Shoot"].triggered)
         {
+            
             float fAngle = isPlayerAiming(playerInput) ? (float)angle : getShootingDirection(charControl);
             Quaternion rotation = Quaternion.Euler(0, 0, fAngle);
-
             Instantiate(bulletPrefab, shootingPoint.position, rotation);
         }
     }
+    
 
     /// <summary>
     ///  <para>Get basic shooting direction if aiming is not happening </para>
