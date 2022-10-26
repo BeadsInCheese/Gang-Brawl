32c7a48d60c6f8eb13c90193b24f855facf73bab
Juuso Rytilahti <rytilahti.juuso@gmail.com>
2022-10-26 10:35:32 +0300

    Objects can be spawned at random intervals #44

--- GangBrawl/Assets/Weapons/WeaponCrate/ObjectSpawner.cs
+++ GangBrawl/Assets/Weapons/WeaponCrate/ObjectSpawner.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class ObjectSpawner : MonoBehaviour
 {
     public GameObject[] spawnObjects;
+    public float minTime;
+    public float maxTime;
+
+    private bool isCycleStarted;
     // Start is called before the first frame update
     void Start()
     {
-        spawnObject();
+        Invoke("spawnObject", 0.5f);
     }
 
     // Update is called once per frame
@@ -19,7 +23,15 @@ public class ObjectSpawner : MonoBehaviour
 
     private void spawnObject()
     {
-        int randomIndex = Random.Range(0, spawnObjects.Length);
-        Instantiate(spawnObjects[randomIndex], this.transform.position, Quaternion.identity);
+        if (isCycleStarted)
+        {
+            int randomIndex = Random.Range(0, spawnObjects.Length);
+            Instantiate(spawnObjects[randomIndex], this.transform.position, Quaternion.identity);
+
+        }
+
+        float nextSpawnIn = Random.Range(minTime, maxTime);
+        Invoke("spawnObject", nextSpawnIn);
+        isCycleStarted = true;
     }
 }
