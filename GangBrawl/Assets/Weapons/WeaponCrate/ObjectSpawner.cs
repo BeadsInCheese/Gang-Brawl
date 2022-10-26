21a070181de41b9ce2cf67620d9f031709ae56c9
Juuso Rytilahti <rytilahti.juuso@gmail.com>
2022-10-26 11:50:15 +0300

    Now spawns objects only if rigidbody is not colliding with spawner #44

--- GangBrawl/Assets/Weapons/WeaponCrate/ObjectSpawner.cs
+++ GangBrawl/Assets/Weapons/WeaponCrate/ObjectSpawner.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Spawns object only if there is nothing colliding with the spawner!
+/// </summary>
 public class ObjectSpawner : MonoBehaviour
 {
     public GameObject[] spawnObjects;
+
+    private List<Collider2D> activelyColliding;
     public float minTime;
     public float maxTime;
-
     private bool isCycleStarted;
+
     // Start is called before the first frame update
     void Start()
     {
         Invoke("spawnObject", 0.5f);
+        activelyColliding = new List<Collider2D>();
     }
 
     // Update is called once per frame
@@ -23,7 +29,7 @@ public class ObjectSpawner : MonoBehaviour
 
     private void spawnObject()
     {
-        if (isCycleStarted)
+        if (isCycleStarted && (activelyColliding.Count == 0))
         {
             int randomIndex = Random.Range(0, spawnObjects.Length);
             Instantiate(spawnObjects[randomIndex], this.transform.position, Quaternion.identity);
@@ -33,5 +39,14 @@ public class ObjectSpawner : MonoBehaviour
         float nextSpawnIn = Random.Range(minTime, maxTime);
         Invoke("spawnObject", nextSpawnIn);
         isCycleStarted = true;
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        activelyColliding.Add(other);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        activelyColliding.Remove(other);
     }
 }
