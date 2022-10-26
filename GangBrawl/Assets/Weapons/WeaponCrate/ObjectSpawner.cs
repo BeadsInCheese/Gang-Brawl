166204dacfd02b16770bf73d1e713530088a0a94
Juuso Rytilahti <rytilahti.juuso@gmail.com>
2022-10-26 12:26:35 +0300

    Bullets are no longer destroyed when they hit the object spawner #44

--- GangBrawl/Assets/Weapons/WeaponCrate/ObjectSpawner.cs
+++ GangBrawl/Assets/Weapons/WeaponCrate/ObjectSpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,16 +10,22 @@ public class ObjectSpawner : MonoBehaviour
 {
     public GameObject[] spawnObjects;
 
+    /// <summary>
+    /// If any items are in this list, the spawner will not spawn an object!
+    /// </summary>
     private List<Collider2D> activelyColliding;
     public float minTime;
     public float maxTime;
     private bool isCycleStarted;
+
+    private BoxCollider2D _collider;
 
     // Start is called before the first frame update
     void Start()
     {
         Invoke("spawnObject", 0.5f);
         activelyColliding = new List<Collider2D>();
+        _collider = gameObject.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -27,26 +34,45 @@ public class ObjectSpawner : MonoBehaviour
 
     }
 
-    private void spawnObject()
-    {
-        if (isCycleStarted && (activelyColliding.Count == 0))
-        {
-            int randomIndex = Random.Range(0, spawnObjects.Length);
-            Instantiate(spawnObjects[randomIndex], this.transform.position, Quaternion.identity);
-
-        }
-
-        float nextSpawnIn = Random.Range(minTime, maxTime);
-        Invoke("spawnObject", nextSpawnIn);
-        isCycleStarted = true;
-    }
     void OnTriggerEnter2D(Collider2D other)
     {
-        activelyColliding.Add(other);
+        // bullets should not stop the crate from spawning
+        if (!isCollidingWithBullet(other.tag))
+        {
+            activelyColliding.Add(other);
+        }
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        activelyColliding.Remove(other);
+        if (!isCollidingWithBullet(other.tag))
+        {
+            activelyColliding.Remove(other);
+        }
     }
+
+    private void spawnObject()
+    {
+        if (isCycleStarted && (activelyColliding.Count == 0))
+        {
+            int randomIndex = UnityEngine.Random.Range(0, spawnObjects.Length);
+            Instantiate(spawnObjects[randomIndex], this.transform.position, Quaternion.identity);
+
+        }
+
+        float nextSpawnIn = UnityEngine.Random.Range(minTime, maxTime);
+        Invoke("spawnObject", nextSpawnIn);
+        isCycleStarted = true;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="str">String to compare</param>
+    /// <returns></returns>
+    private bool isCollidingWithBullet(String str)
+    {
+        return String.Equals(str, "Bullet");
+    }
+
 }
