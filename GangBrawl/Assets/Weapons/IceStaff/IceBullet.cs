63a99efe1b54c51e7d7371652294a770e1e15e5d
BeadsInCheese <36571102+BeadsInCheese@users.noreply.github.com>
2022-11-06 04:57:07 +0200

    IceStaff added #62

---
+++ GangBrawl/Assets/Weapons/IceStaff/IceBullet.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceBullet : Bullet
+{
+    // Start is called before the first frame update
+    public GameObject IceWall;
+        void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag.Equals("Platform")){
+            var wall = Instantiate(IceWall); 
+            wall.transform.position=this.transform.position;  
+        }
+        if(collision.tag.Equals("Player")){
+            //Maybe move speed control to funtion and raise it after time with invoke?
+            CharacterControl characterController=collision.gameObject.GetComponent<CharacterControl>();
+            characterController.speed=Mathf.Clamp(characterController.speed-1,1,characterController.maxMovementSpeed);
+        }
+        Helpers.HitPlayer(damage,  collision.gameObject,rb.velocity.normalized*knockback);
+        if (!collision.gameObject.tag.Equals("Bullet") && !collision.tag.Equals("ObjectSpawner"))
+        {
+            Destroy(gameObject);
+        }
+
+
+
+        
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        // The if clause is directly taken from diesOutOfBounds 
+        if (Helpers.isOutOfArena(gameObject))
+        {
+            Destroy(gameObject);
+        }
+    
+    }
+}
---
+++ GangBrawl/Assets/Weapons/IceStaff/Icewall.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Icewall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject,60);
+    }
+
+    // Update is called once per frame
+
+}
