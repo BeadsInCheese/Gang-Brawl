fe8d202eca88f287a428916a971f6d79c08d91ff
BeadsInCheese <36571102+BeadsInCheese@users.noreply.github.com>
2022-11-08 03:07:57 +0200

    AI shootscript base #55

---
+++ GangBrawl/Assets/AI/AIShoot.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIShoot : Shoot
+{
+    // Start is called before the first frame update
+    void Update(){}
+}
