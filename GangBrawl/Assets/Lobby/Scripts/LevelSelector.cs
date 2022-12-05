7b7cfc50f4a633de8cd9ef3445a20c879f83103a
Juuso Rytilahti <rytilahti.juuso@gmail.com>
2022-12-05 07:47:47 +0200

    Mode change is displayed in the lobby menu #78
    - Can currently only be changed from the R2 of the controller

--- GangBrawl/Assets/Lobby/Scripts/LevelSelector.cs
+++ GangBrawl/Assets/Lobby/Scripts/LevelSelector.cs
@@ -1,24 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 using UnityEngine.InputSystem;
 public class LevelSelector : MonoBehaviour
 {
+    public TMP_Text modeLabel;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
-    int modeCounter=0;
-    private DirectorBehaviour.Gamemode modeChange(){
-        DirectorBehaviour.Gamemode[] modes={DirectorBehaviour.Gamemode.DEATHMATCH,DirectorBehaviour.Gamemode.LASTMANSTANDING};
-        modeCounter=(modeCounter+1)%modes.Length;
-        DirectorBehaviour.Gamemode mode=modes[modeCounter];
-        DirectorBehaviour.gameMode=mode;
+    int modeCounter = 0;
+    public DirectorBehaviour.Gamemode modeChange()
+    {
+        DirectorBehaviour.Gamemode[] modes = { DirectorBehaviour.Gamemode.DEATHMATCH, DirectorBehaviour.Gamemode.LASTMANSTANDING };
+        modeCounter = (modeCounter + 1) % modes.Length;
+        DirectorBehaviour.Gamemode mode = modes[modeCounter];
+        DirectorBehaviour.gameMode = mode;
+        // change the mode button text in the settings
+        UpdateModeLabel();
         return mode;
+    }
+
+    public void UpdateModeLabel()
+    {
+        // Not entirely sure why this requires a null check
+        if (modeLabel != null)
+        {
+            Debug.Log("ModeLabel was updated and was not null");
+            if (DirectorBehaviour.gameMode == DirectorBehaviour.Gamemode.DEATHMATCH)
+            {
+                modeLabel.text = "Deathmatch";
+            }
+            else if (DirectorBehaviour.gameMode == DirectorBehaviour.Gamemode.LASTMANSTANDING)
+            {
+                modeLabel.text = "Last Man Standing";
+            }
+        }
     }
     void Update()
     {
