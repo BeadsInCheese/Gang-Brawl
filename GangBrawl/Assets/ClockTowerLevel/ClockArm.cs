8ab700854716689614c210dc26047b95551a1e5a
BeadsInCheese <36571102+BeadsInCheese@users.noreply.github.com>
2023-01-06 20:32:06 +0200

    Added missing systems to clocktower level

--- GangBrawl/Assets/ClockTowerLevel/ClockArm.cs
+++ GangBrawl/Assets/ClockTowerLevel/ClockArm.cs
@@ -14,6 +14,6 @@ public class ClockArm : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(spinSpeed);
+        transform.Rotate(spinSpeed*Time.deltaTime);
     }
 }
