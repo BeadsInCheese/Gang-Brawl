8edd5b8cddc1090b528401714c34abf4aa80564f
BeadsInCheese <36571102+BeadsInCheese@users.noreply.github.com>
2023-01-08 22:12:21 +0200

    Added scene changes #126

--- GangBrawl/Assets/balloon.cs
+++ GangBrawl/Assets/balloon.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class balloon : MonoBehaviour
 {
     public GameObject Pop;
-    // Start is called before the first frame update
+    public GameObject basket;
+    private Rigidbody2D basketRB;
+        // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D col){
         if( col.gameObject.tag.Equals("Bullet")){
             Instantiate(Pop).transform.position=this.transform.position;
             Destroy(gameObject);
+            //basketRB.gravityscale=1;
+            
+            //Destroy(gameObject);
         }
     }
     void Start()
     {
-        
+    basketRB=basket.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
