cf6e54c8934acb3782bd11c764b0c6d2347aab74
Juuso Rytilahti <rytilahti.juuso@gmail.com>
2023-01-19 16:04:51 +0200

    PlayerData has now enum for different controller types #100

--- GangBrawl/Assets/Lobby/Scripts/PlayerData.cs
+++ GangBrawl/Assets/Lobby/Scripts/PlayerData.cs
@@ -1,16 +1,38 @@
 using System.Collections;
 using UnityEngine.InputSystem;
+using UnityEngine;
 public class PlayerData
 {
     public string characterPrefabPath;
     public int playerIndex;
     public string controlScheme;
     public InputDevice[] pairWithDevices;
-    public PlayerData(string characterPrefab, int playerIndex,string controlScheme,InputDevice[] devices){
-        characterPrefabPath=characterPrefab;
-        this.playerIndex=playerIndex;
-        this.controlScheme=controlScheme;
-        this.pairWithDevices=devices;
+
+    public ControllerType controllerType;
+    public PlayerData(string characterPrefab, int playerIndex, string controlScheme, InputDevice[] devices)
+    {
+        characterPrefabPath = characterPrefab;
+        this.playerIndex = playerIndex;
+        this.controlScheme = controlScheme;
+        this.pairWithDevices = devices;
+        // I could swear that my ps5 controller gave one time it's type as "wireless controller", not sure why
+        if (controlScheme.ToLower().Contains("controller") || controlScheme.ToLower().Contains("gamepad"))
+        {
+            this.controllerType = ControllerType.Gamepad;
+        }
+        else if (controlScheme.ToLower().Contains("keyboard"))
+        {
+            this.controllerType = ControllerType.Keyboard;
+        }
 
     }
+}
+
+/// <summary>
+/// There are currently two controllertypes, a gamepad (e.g. ps5 controller) called gamepad, and Keyboard for Keyboard.
+/// </summary>
+public enum ControllerType
+{
+    Gamepad,
+    Keyboard
 }
