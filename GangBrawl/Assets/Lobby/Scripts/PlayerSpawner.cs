354a6045eab3ca72f4ab0655b921777d36ef472a
Juuso Rytilahti <rytilahti.juuso@gmail.com>
2023-04-21 15:18:28 +0300

    created static colors list #93

--- GangBrawl/Assets/Lobby/Scripts/PlayerSpawner.cs
+++ GangBrawl/Assets/Lobby/Scripts/PlayerSpawner.cs
@@ -7,39 +7,40 @@ public class PlayerSpawner : MonoBehaviour
 {
     public GameObject PlayerCPU;
     PlayerInputManager playerInputManager;
-    public List<Color> PlayerColorTints=new List<Color>();
-    public void spawnPlayers(){
-        int playerNo=0;
-        foreach(PlayerData i in LobbyManager.playerData){
+    public void spawnPlayers()
+    {
+        int playerNo = 0;
+        foreach (PlayerData i in LobbyManager.playerData)
+        {
 
-            var player=playerInputManager.JoinPlayer(playerNo,-1,null,i.pairWithDevices);
-            player.gameObject.GetComponent<CharacterControl>().tint=PlayerColorTints[playerNo];
-            playerNo=playerNo+1;
-            
+            var player = playerInputManager.JoinPlayer(playerNo, -1, null, i.pairWithDevices);
+            player.gameObject.GetComponent<CharacterControl>().tint = PlayerColors.Colors[playerNo];
+            playerNo = playerNo + 1;
+
         }
-        for(int i=0; i<LobbyManager.CPUCount; i++){
-            var c=Instantiate(PlayerCPU);
-            var ai=c.GetComponent<AICharacter>();
-            ai.tint=PlayerColorTints[playerNo];
-            ai.index=playerNo;
-            playerNo+=1;
+        for (int i = 0; i < LobbyManager.CPUCount; i++)
+        {
+            var c = Instantiate(PlayerCPU);
+            var ai = c.GetComponent<AICharacter>();
+            ai.tint = PlayerColors.Colors[playerNo];
+            playerNo += 1;
         }
-        LobbyManager.CPUCount=0;
-        LobbyManager.playerData=new List<PlayerData>();
-        
+        LobbyManager.CPUCount = 0;
+        LobbyManager.playerData = new List<PlayerData>();
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        playerInputManager=GetComponent<PlayerInputManager>();
+        playerInputManager = GetComponent<PlayerInputManager>();
         spawnPlayers();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    
+
 }
