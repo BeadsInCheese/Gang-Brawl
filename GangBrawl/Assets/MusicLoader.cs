0dd1f1b8cf616277873b307abc8d143dc30f2595
BeadsInCheese <hirvittavanhauskaa@gmail.com>
2023-05-04 22:21:05 +0300

    Made punch more quiet and bassy and added ambiant layer to audio

--- GangBrawl/Assets/MusicLoader.cs
+++ GangBrawl/Assets/MusicLoader.cs
@@ -6,9 +6,18 @@ public class MusicLoader : MonoBehaviour
 {
     // Start is called before the first frame update
     public int key = 0;
+    public AudioClip ambiant;
     void Start()
     {
-        AudioManager.instance.changeMusic(key);        
+        AudioManager.instance.changeMusic(key);
+        if (ambiant != null)
+        {
+            AudioManager.instance.StartAmbiant(ambiant);
+        }
+        else
+        {
+            AudioManager.instance.StopAmbiant();
+        }
     }
 
 
