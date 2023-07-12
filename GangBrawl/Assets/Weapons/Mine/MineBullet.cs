2b06f709c0607e1218853c7b65ceef889244c63d
BeadsInCheese <hirvittavanhauskaa@gmail.com>
2023-07-12 16:42:53 +0300

    Added weapon: mine

---
+++ GangBrawl/Assets/Weapons/Mine/MineBullet.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineBullet :  Bullet
+{
+    public GameObject explosion;
+    // Start is called before the first frame update
+    public float activationTime = 4;
+    bool activated=false;
+    public void activate()
+    {
+        activated = true;
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Invoke("activate", activationTime);
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((collision.gameObject.tag.Equals("Player") || collision.gameObject.tag.Equals("ExplosivesBarrel"))&&activated)
+        {
+            var ex = Instantiate(explosion);
+            Helpers.HitPlayer(damage, collision.gameObject, rb.velocity.normalized * knockback);
+            ex.transform.position = new Vector2(transform.position.x, transform.position.y);
+            Destroy(gameObject);
+        }
+
+
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Helpers.isOutOfArena(gameObject))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
