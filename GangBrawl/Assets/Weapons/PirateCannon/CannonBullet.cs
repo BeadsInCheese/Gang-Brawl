8f8c2346efcb6936bf28c3fac08135effe8971a0
BeadsInCheese <hirvittavanhauskaa@gmail.com>
2023-07-27 06:18:17 +0300

    Added pirate cannon

---
+++ GangBrawl/Assets/Weapons/PirateCannon/CannonBullet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBullet : Bullet
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            var p = collision.gameObject;
+            if (p.GetComponent<HPSystem>().currentHp - damage <= 0)
+            {
+                DirectorBehaviour.PlayerKills[owner] += 1;
+                //Debug.Log(owner+ " has "+DirectorBehaviour.PlayerKills[owner]+" kills.");
+            }
+
+        }
+        Helpers.HitPlayer(damage, collision.gameObject, rb.velocity.normalized * knockback);
+
+
+        if (collision.gameObject.tag.Equals("Platform") )
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+}
